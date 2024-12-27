1e0818698b5ac3f2760f8bdc51b87e612b253d99
Bhuvanakrishnan0102 <131353623+Bhuvanakrishnan0102@users.noreply.github.com>
2024-12-27 18:37:45 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,21 @@
+public class SimpleProgram {
+    public static void main(String[] args) {
+       
+        System.out.println("Welcome to the Simple Java Program!");
+
+        // Declare and initialize variables
+        int number1 = 10;
+        int number2 = 20;
+
+        // Perform addition
+        int sum = number1 + number2;
+
+        // Print the numbers and their sum
+        System.out.println("First number: " + number1);
+        System.out.println("Second number: " + number2);
+        System.out.println("Sum: " + sum);
+
+        // End of the program
+        System.out.println("Thank you for running this program!");
+    }
+}
